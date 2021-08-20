f46fc17b622709ac19fea1f519c7cbaf02b36e4a
Олег Сидорин <oleg.sidorin@gmail.com>
2021-08-20 23:19:48 +0300

    Добавьте файлы проекта.

---
+++ INum/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace INum
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Main : IExternalApplication
+    {
+        public static string TabName { get; set; } = "Надстройки";
+        public static string PanelTechName { get; set; } = "NUMINC";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            var techPanel = application.CreateRibbonPanel(PanelTechName);
+            string path = Assembly.GetExecutingAssembly().Location;
+
+            var MBtnData = new PushButtonData("MBtnData", "Авто\nномер", path, "INum.NumCommand")
+            {
+                ToolTipImage = new BitmapImage(new Uri(Path.GetDirectoryName(path) + "\\res\\num.png", UriKind.Absolute)),
+                ToolTip = "Маркирует экземпляры семейств"
+            };
+            var TechBtn = techPanel.AddItem(MBtnData) as PushButton;
+            TechBtn.LargeImage = new BitmapImage(new Uri(Path.GetDirectoryName(path) + "\\res\\num-32.png", UriKind.Absolute));
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ INum/NumCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace INum
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    class NumCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            System.Windows.Forms.MessageBox.Show("Привет! " + doc.ActiveView.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
